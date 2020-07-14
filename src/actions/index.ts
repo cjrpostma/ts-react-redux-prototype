f0ea5546d31e55c25ea2ad70bdf9425d33e93c7b
Chris Postma <cjrpostma@gmail.com>
2020-07-14 17:15:29 -0400

    Add thunk to fetch todos from api.

---
+++ src/actions/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { ActionTypes } from './types';
+import { Dispatch } from 'redux';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    });
+  };
+};
